661e01e52f99d3022766c0c53040fbef34d4cd87
Mohamed Elkilany <mosayed050@gmail.com>
2021-09-09 16:20:20 +0200

    Feat/Create Extension folder and adding UIView Controller Ext

---
+++ MoviesApp/Extenstions/ViewController+Ext.swift
@@ -0,0 +1,45 @@
+//
+//  UIViewController+Ext.swift
+//  MoviesApp
+//
+//  Created by Mohamed Elkilany on 09/09/2021.
+//
+
+import UIKit
+
+extension UIViewController {
+
+func scrollToTop() {
+       func scrollToTop(view: UIView?) {
+           guard let view = view else { return }
+
+           switch view {
+           case let scrollView as UIScrollView:
+               if scrollView.scrollsToTop == true {
+                   scrollView.setContentOffset(CGPoint(x: 0.0, y: -scrollView.contentInset.top), animated: true)
+                   return
+               }
+           default:
+               break
+           }
+
+           for subView in view.subviews {
+               scrollToTop(view: subView)
+           }
+       }
+       scrollToTop(view: view)
+   }
+
+   var isScrolledToTop: Bool {
+       if self is UITableViewController {
+        return (self as! UITableViewController).tableView.contentOffset.y == 0
+       }
+    
+       for subView in view.subviews {
+           if let scrollView = subView as? UIScrollView {
+               return (scrollView.contentOffset.y == 0)
+           }
+       }
+       return true
+   }
+}
