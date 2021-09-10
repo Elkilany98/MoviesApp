5ed9bd2f2f308acfa6a6767f29ddb20864c905f5
Mohamed Elkilany <mosayed050@gmail.com>
2021-09-10 18:09:27 +0200

    Feat/Adding network get top rated functionality in protocol Network manager

--- MoviesApp/NetworkManager/NetworkManager.swift
+++ MoviesApp/NetworkManager/NetworkManager.swift
@@ -10,9 +10,11 @@ import Alamofire
 
 protocol NetworkManagerProtocol {
     func getNowPlayingMoviesNetwork(page:String,completion:@escaping (AFResult<NowPlayingModel>)->Void)
+    func getTopRatedMoviesNetwork(page:String,completion:@escaping (AFResult<TopRatedModel>)->Void)
 }
 
 class  NetworkManager : NetworkManagerProtocol {
+    
     func getNowPlayingMoviesNetwork(page: String, completion: @escaping (AFResult<NowPlayingModel>) -> Void) {
         AF.request(Router.nowPlayingMovies(page: page))
             .responseDecodable { (response: AFDataResponse<NowPlayingModel>) in
@@ -20,5 +22,15 @@ class  NetworkManager : NetworkManagerProtocol {
                 print("full Response = \(response.result)")
             }
     }
+    
+    func getTopRatedMoviesNetwork(page: String, completion: @escaping (AFResult<TopRatedModel>) -> Void) {
+        AF.request(Router.topRatedMovies(page: page))
+            .responseDecodable { (response: AFDataResponse<TopRatedModel>) in
+                completion(response.result)
+                print("full Response = \(response.result)")
+            }
+    }
+    
+    
 }
 
