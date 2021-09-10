e9c5876fcac36f3997aa693ef47964e35631efc6
Mohamed Elkilany <mosayed050@gmail.com>
2021-09-10 18:06:50 +0200

    Feat/adding Top reted Movies list model

--- MoviesApp/Scences/TapBarScreens/TopRated/Model/TopRatedModel.swift
+++ MoviesApp/Scences/TapBarScreens/TopRated/Model/TopRatedModel.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
-class TopRatedViewModel{
-    
+
+struct TopRatedModel: Codable {
+    let page: Int?
+    let results: [TopRatedList]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
 }
 
+struct TopRatedList: Codable {
+    let id: Int?
+    let originalTitle: String?
+    let voteAverage: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case originalTitle = "original_title"
+        case voteAverage = "vote_average"
+  
+    }
+}
