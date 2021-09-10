771d35266552d1f85703f5a3de3de78c3f3ef86b
Mohamed Elkilany <mosayed050@gmail.com>
2021-09-10 20:49:04 +0200

    Feat/Complete search bar functionality and access to text

--- MoviesApp/Scences/TapBarScreens/Search/SearchModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SearchModel.swift
-//  MoviesApp
-//
-//  Created by Mohamed Elkilany on 09/09/2021.
-//
-
-import Foundation
--- MoviesApp/Scences/TapBarScreens/Search/SearchVC.swift
+++ MoviesApp/Scences/TapBarScreens/Search/SearchVC.swift
@@ -9,16 +9,26 @@ import UIKit
 
 class SearchVC: UIViewController {
 
-     let searchController = UISearchController()
+    let searchController = UISearchController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Search"
         searchController.searchBar.placeholder = "Movies"
         navigationController?.navigationBar.prefersLargeTitles = true
+        searchController.searchResultsUpdater = self
         navigationItem.searchController = searchController
         searchController.searchBar.showsCancelButton = true
         searchController.searchBar.tintColor = #colorLiteral(red: 0.09411764706, green: 0.1568627451, blue: 0.5647058824, alpha: 1)
     }
+}
 
+
+extension SearchVC : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let text = searchController.searchBar.text  {
+            print("the text is ", text)
+        }
+        
+    }
 }
